0bf327a4c0204272911b49a71b8632a7d478889e
pranab56 <pronabhalder53@gmail.com>
2022-02-17 00:53:45 +0600

    function

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+function getTotalExpenses(expenses){
+    const food=document.getElementById(expenses+'-input');
+    const foodTotal=food.value;
+    const totalFood=parseFloat(foodTotal)
+    return totalFood
+}
 
 
 
@@ -7,20 +12,23 @@
 
 
 document.getElementById('claculate').addEventListener('click',function(){
-   const food=document.getElementById('food-input');
-   const foodTotal=food.value;
-   const totalFood=parseFloat(foodTotal)
+  const totalFood=  getTotalExpenses('food')
 
-   const rent=document.getElementById('rent-input');
-   const rentTotal=rent.value;
-   const totalrent=parseFloat(rentTotal);
+//    const rent=document.getElementById('rent-input');
+//    const rentTotal=rent.value;
+//    const totalrent=parseFloat(rentTotal);
+    const totalrent= getTotalExpenses('rent')
 
-   const colthes=document.getElementById('clothes-input');
-   const colthesTotal=colthes.value;
-   const totalColthes=parseFloat(colthesTotal)
+//    const colthes=document.getElementById('clothes-input');
+//    const colthesTotal=colthes.value;
+//    const totalColthes=parseFloat(colthesTotal)
+const totalColthes= getTotalExpenses('rent')
+
 
    const totalexpenses=document.getElementById('total-expenses');
   totalexpenses.innerText=totalFood+totalrent+totalColthes
+
+ 
 
 
 })
