5201cd02192a516bc656f8281435276bac10f3ba
pranab56 <pronabhalder53@gmail.com>
2022-02-17 09:49:08 +0600

    fuction

--- index.js
+++ index.js
@@ -8,28 +8,24 @@ function getInputTotal(getId){
 
 
 document.getElementById('claculate').addEventListener('click',function(){
-  const foodTotal=getInputTotal('food-input')
-  const rentTotal=getInputTotal('rent-input')
-  const clothesTotal=getInputTotal('clothes-input')
-  const total=foodTotal+rentTotal+clothesTotal
-  if(total>0){
-
-    const totalexpenses=document.getElementById('total-expenses');
-    totalexpenses.innerText=total
-    
-   const income=getInputTotal('income-input')
-   if(income>0){
-    const balance=income-total;
-     
-    const BalanceInput=document.getElementById('balance');
-      BalanceInput.innerText=balance
-   }
+ 
   
-  }
-  else{
-    alert('Your number is incorrect.Please enter the correct number!!')
-  }
-
-
-  
+      const income=getInputTotal('income-input')
+      const foodTotal=getInputTotal('food-input')
+        const rentTotal=getInputTotal('rent-input')
+        const clothesTotal=getInputTotal('clothes-input')
+      if(income>0 && foodTotal>0 && rentTotal>0 && clothesTotal>0){
+        const total=foodTotal+rentTotal+clothesTotal
+        if(income>total){
+          const totalexpenses=document.getElementById('total-expenses');
+          totalexpenses.innerText=total;
+       const balance=income-total;
+      
+        const BalanceInput=document.getElementById('balance');
+        BalanceInput.innerText=balance
+        }  
+      }
+      else{
+        alert('Your number is incorrect. Please enter the correct number')
+      }
 })
