0d4d7cfdc6f2d8b0621340fafb0b62eb4e8782be
pranab56 <pronabhalder53@gmail.com>
2022-02-17 15:00:55 +0600

    error

--- index.js
+++ index.js
@@ -1,57 +1,50 @@
 function getInputTotal(getId){
   const input=document.getElementById(getId);
   const TotalInput=parseFloat(input.value);
-  input.value='';
   return TotalInput
 }
 
-
-
 document.getElementById('claculate').addEventListener('click',function(){
- 
-  
       const income=getInputTotal('income-input')
       const foodTotal=getInputTotal('food-input')
         const rentTotal=getInputTotal('rent-input')
         const clothesTotal=getInputTotal('clothes-input')
       if(income>0 && foodTotal>0 && rentTotal>0 && clothesTotal>0){
         const total=foodTotal+rentTotal+clothesTotal
-        if(income>total){
+        if(income>total && income>total){
           const totalexpenses=document.getElementById('total-expenses');
-          totalexpenses.innerText=total;
-       const balance=income-total;
-      
-        const BalanceInput=document.getElementById('balance');
-        BalanceInput.innerText=balance
-
+            totalexpenses.innerText=total;
+            const balance=income-total;
+           
+             const BalanceInput=document.getElementById('balance');
+             BalanceInput.innerText=balance
+        
         document.getElementById('save').addEventListener('click',function(){
-
-
-          
           const saveInputTotal=getInputTotal('saveInput')
-          
           if(saveInputTotal>0){
-            saveInput.value=balance / (Math.round(saveInputTotal));
-            
+            saveInput.value=(income/100 )* saveInputTotal;
           }
           else{
-            alert('Your number is incorrect. Please enter the correct number')
+            alert('Your number is incorrect. Please enter the correct number!!')
           }
           
           const savingAmount=document.getElementById('saving-amount');
           savingAmount.innerText=saveInput.value
           const remainingBalance=document.getElementById('remaining-balance');
-          remainingBalance.innerText=balance - (Math.round(saveInput.value) );
+          remainingBalance.innerText=balance - saveInput.value;
           saveInput.value='';
-         
         })
-        }  
+        } 
+        else{
+          alert("sorry!! income can't be less then expenses. ")
+        } 
       }
       else{
-        alert('Your number is incorrect. Please enter the correct number')
-      }
-      
+        alert('Your number is incorrect. Please enter the correct number!!')
+      } 
 })
+
+
 
 
 
